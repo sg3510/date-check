b1918a2cd6d430bbfcfae6f478ab85e35dfc9d30
Seb Grubb <sg3510@ic.ac.uk>
2015-03-23 16:52:27 +0000

    example update

--- get_dates.py
+++ get_dates.py
@@ -8,57 +8,86 @@ import random
 with open('settings.conf') as data_file:
 	conf = json.load(data_file)
 
+def get_dates():
+	br = mechanize.Browser()
+	br.set_handle_robots(False)
+	br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+	br.open("https://driverpracticaltest.direct.gov.uk/application?execution=e1s1")
 
-br = mechanize.Browser()
-br.set_handle_robots(False)
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-br.open("https://driverpracticaltest.direct.gov.uk/application?execution=e1s1")
-
-# Wait random period to avoid CAPTCHAs
-time.sleep(1 + random.randrange(12,245)/100.0)
+	# Wait random period to avoid CAPTCHAs
+	time.sleep(1 + random.randrange(12,245)/100.0)
 
 
-br.select_form(nr=0)
-br.form.find_control(name="testTypeCar", id='test-type-car')
-br.method = "POST"
-br.submit()
-print br.geturl()
+	br.select_form(nr=0)
+	br.form.find_control(name="testTypeCar", id='test-type-car')
+	br.method = "POST"
+	br.submit()
+	print br.geturl()
 
-time.sleep(1 + random.randrange(12,245)/100.0)
+	time.sleep(1 + random.randrange(12,245)/100.0)
 
-br.form = list(br.forms())[0]
-br['driverLicenceNumber'] = conf['driverLicenceNumber']
-br['specialNeeds'] = ['false']
-resp = br.submit()
-print br.geturl()
+	br.form = list(br.forms())[0]
+	br['driverLicenceNumber'] = conf['driverLicenceNumber']
+	br['specialNeeds'] = ['false']
+	resp = br.submit()
+	print br.geturl()
 
-time.sleep(1 + random.randrange(12,245)/100.0)
+	time.sleep(1 + random.randrange(12,245)/100.0)
 
-br.form = list(br.forms())[0]
-br['testCentreName'] = conf['testCentreName']
-br.submit()
-print br.geturl()
+	br.form = list(br.forms())[0]
+	br['testCentreName'] = conf['testCentreName']
+	br.submit()
+	print br.geturl()
 
-time.sleep(1 + random.randrange(12,245)/100.0)
+	time.sleep(1 + random.randrange(12,245)/100.0)
 
-link = br.find_link(text_regex=r"Choose")
-br.open(br.click_link(link))
-print br.geturl()
+	link = br.find_link(text_regex=r"Choose")
+	br.open(br.click_link(link))
+	print br.geturl()
 
-time.sleep(1 + random.randrange(12,245)/100.0)
+	time.sleep(1 + random.randrange(12,245)/100.0)
 
-br.form = list(br.forms())[0]
-br['preferredTestDate'] = conf['preferredTestDate']
-response = br.submit()
-soup = BeautifulSoup(response.read())
+	br.form = list(br.forms())[0]
+	br['preferredTestDate'] = conf['preferredTestDate']
+	response = br.submit()
+	soup = BeautifulSoup(response.read())
 
-dt_min = datetime.datetime(conf['minYear'], conf['minMonth'], conf['minDay'])
-dt_max = datetime.datetime(conf['maxYear'], conf['maxMonth'], conf['maxDay'])
+	dt_min = datetime.datetime(conf['minYear'], conf['minMonth'], conf['minDay'])
+	dt_max = datetime.datetime(conf['maxYear'], conf['maxMonth'], conf['maxDay'])
 
-for a in soup('ul')[1].findAll('span'):
-	# time.strptime(a.contents[0], "%A %d %B %Y %I:%M%p")
-	dt = datetime.datetime(*time.strptime(a.contents[0], "%A %d %B %Y %I:%M%p")[:6])
-	if (dt_min < dt) and (dt_max > dt):
-		print "found!"
-		print a.contents[0]
-	# print a.contents[0]
+	for a in soup('ul')[1].findAll('span'):
+		# time.strptime(a.contents[0], "%A %d %B %Y %I:%M%p")
+		dt = datetime.datetime(*time.strptime(a.contents[0], "%A %d %B %Y %I:%M%p")[:6])
+		if (dt_min < dt) and (dt_max > dt):
+			print "found!"
+			print a.contents[0]
+		# print a.contents[0]
+
+def send_mail():
+	import smtplib
+	from email.MIMEMultipart import MIMEMultipart
+	from email.MIMEText import MIMEText
+	msg = MIMEMultipart()
+	msg['From'] = 'me@gmail.com'
+	msg['To'] = 'you@gmail.com'
+	msg['Subject'] = 'simple email in python'
+	message = 'here is the email'
+	msg.attach(MIMEText(message))
+
+	mailserver = smtplib.SMTP('smtp.gmail.com',587)
+	# identify ourselves to smtp gmail client
+	mailserver.ehlo()
+	# secure our email with tls encryption
+	mailserver.starttls()
+	# re-identify ourselves as an encrypted connection
+	mailserver.ehlo()
+	mailserver.login('me@gmail.com', 'mypassword')
+
+	mailserver.sendmail('me@gmail.com','you@gmail.com',msg.as_string())
+
+	mailserver.quit()
+
+
+
+if __name__ == '__main__':
+	get_dates()
